a5bbebf879a84a6ef731d7e62ad9f5ea9febcd4c
fawwasaldy <fawwas.aldy2111@gmail.com>
2026-02-18 14:37:31 +0700

    feat: handle model not found exceptions with custom JSON response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Middleware\EnsureApiRequestsAcceptJson;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,17 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $e) {
+            $previous = $e->getPrevious();
+
+            if (! $previous instanceof ModelNotFoundException) {
+                return null;
+            }
+
+            $modelName = class_basename($previous->getModel());
+
+            return response()->json([
+                'message' => "{$modelName} does not exist.",
+            ], 404);
+        });
     })->create();
